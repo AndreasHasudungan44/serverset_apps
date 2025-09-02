ca9c0680be0e0b8ffb4ed159b02eaa47769a37c3
Andreas_Priambodo <148003770+AndreasHasudungan44@users.noreply.github.com>
2025-09-02 17:33:03 +0200

    Update setup.sh
    Updated labelling, removed automatic updating for all system structure

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e  # Exit on any error
 
-# --- Check for dnf (Red Hat family) ---
+#### check if dnf is installed ####
 if ! command -v dnf &> /dev/null; then
     echo -e "\e[31mThis script requires a Red Hat-based system with 'dnf' installed.\e[0m"
     exit 1
@@ -10,15 +10,15 @@ fi
 
 echo "Installing system packages..."
 
-sudo dnf update -y
+
 sudo dnf install -y \
     wget curl git gcc gcc-c++ make \
     openssl-devel libcurl-devel libxml2-devel \
     zlib-devel libpng-devel \
     python3 python3-pip python3-venv R
 
-# --- Python virtual environment ---
-echo "Setting up Python virtual environment..."
+#### PYTHON 3 ######
+echo "Setting up Python virtual environment"
 
 python3 -m venv ~/quarto-env
 source ~/quarto-env/bin/activate
@@ -26,14 +26,14 @@ source ~/quarto-env/bin/activate
 pip install --upgrade pip
 pip install jupyter pandas matplotlib quarto
 
-# --- Quarto CLI installation ---
+### QUARTO CLI ####
 echo "Installing Quarto CLI..."
 
 wget https://quarto.org/download/latest/quarto-linux-amd64.rpm
 sudo dnf install -y ./quarto-linux-amd64.rpm
 rm -f quarto-linux-amd64.rpm
 
-# --- R package installation ---
+#### R PACKAGELIST ####
 echo "Installing R packages..."
 
 Rscript -e "install.packages(c('languageserver', 'tidyverse', 'reticulate', 'rmarkdown', 'knitr'), repos='https://cloud.r-project.org/')"
